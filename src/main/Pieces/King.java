6c2300078c479cae7b51d1180eb5a53245ba83d2
saptarshi3112 <senguptasaptarshi11@gmail.com>
2020-01-13 19:50:00 +0530

    king init

---
+++ src/main/Pieces/King.java
@@ -0,0 +1,23 @@
+package main.Pieces;
+
+import main.Board.Tile;
+import main.Utility.Index;
+import main.Utility.Team;
+
+import java.util.ArrayList;
+
+public class King extends Piece {
+    public King(String name, String imgPath, Team color, String category) {
+        super(name, imgPath, color, category);
+    }
+
+    @Override
+    public ArrayList<Index> calculatePossibleMoves(int x, int y, Tile[][] tileBoard, Tile currentTile) {
+        return null;
+    }
+
+    @Override
+    public Boolean isSafe(int x, int y, Tile[][] tileBoard, Tile currentTile) {
+        return null;
+    }
+}
